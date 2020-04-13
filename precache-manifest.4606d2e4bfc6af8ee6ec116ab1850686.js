ae6d31cdbdfd01daade7dbac7506b4a9fa7cf5e6
marshadow22 <56247202+marshadow22@users.noreply.github.com>
2020-04-13 14:51:50 -0500

    Updates

---
+++ precache-manifest.4606d2e4bfc6af8ee6ec116ab1850686.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c605475668a2b75f76c6e75e23a4b4c1",
+    "url": "/Primalpleasureapp/index.html"
+  },
+  {
+    "revision": "0ded450453189ca05e36",
+    "url": "/Primalpleasureapp/static/css/2.1417c86a.chunk.css"
+  },
+  {
+    "revision": "33b5a1b7604decb652b6",
+    "url": "/Primalpleasureapp/static/css/main.4000e68a.chunk.css"
+  },
+  {
+    "revision": "0ded450453189ca05e36",
+    "url": "/Primalpleasureapp/static/js/2.127b08b1.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/Primalpleasureapp/static/js/2.127b08b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33b5a1b7604decb652b6",
+    "url": "/Primalpleasureapp/static/js/main.cc4e65f4.chunk.js"
+  },
+  {
+    "revision": "8d83938811c7cdb69fbd",
+    "url": "/Primalpleasureapp/static/js/runtime-main.aae9c4a7.js"
+  },
+  {
+    "revision": "f5abb3d615fac87fd5833369c162ebff",
+    "url": "/Primalpleasureapp/static/media/logo.f5abb3d6.png"
+  }
+]);
